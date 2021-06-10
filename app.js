b79db464557ff29083e2ff6bb9025c549fcf1bb4
Sarunas <sarunas.lekstutis@gmail.com>
2021-06-10 22:00:18 +0300

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
- console.log(req.url, req.method, req.headers);
+ const url = req.url;
+ const method = req.method;
+ if (url === '/') {
+  res.write('<html>');
+  res.write('<head><title>Enter message</title></head>');
+  res.write('<body><form method="POST" action="/message"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
+  res.write('</html>');
+  return res.end();
+ }
+ if (url === '/message' && method === "POST") {
+  const body = [];
+  req.on('data', (chunk) => {
+   body.push(chunk);
+  });
+  req.on('end', () => {
+   const parsedBody = Buffer.concat(body).toString();
+   const message = parsedBody.split('=')[1];
+   fs.writeFileSync('message.txt', message);
+  });
+  res.statusCode = 302;
+  res.setHeader('Location', '/');
+  return res.end();
+ }
  res.setHeader('Content-type', 'text/html');
  res.write('<html>');
  res.write('<head><title>My First Page</title></head>');
